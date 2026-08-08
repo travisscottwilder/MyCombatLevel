4bd7d58005f772f3293ec970e37783a0b34284db
unknown <travisscottwilder@gmail.com>
2026-08-08 16:36:39 -0700

    Saving work

--- src/main/java/com/mycombatlevel/ExampleConfig.java
+++ src/main/java/com/mycombatlevel/ExampleConfig.java
@@ -3,17 +3,183 @@ package com.mycombatlevel;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
+
+import java.awt.*;
 
 @ConfigGroup("mycombatlevel")
-public interface ExampleConfig extends Config
-{
-	@ConfigItem(
-			keyName = "highlightPlayers",
-			name = "Highlight Attackable Players",
-			description = "Highlight players within your combat range"
-	)
-	default boolean highlightPlayers()
-	{
-		return true;
+public interface ExampleConfig extends Config {
+
+	enum HighlightMode {
+		ENEMIES_ARE_NEAR,
+		ALWAYS
 	}
+
+
+	@ConfigSection(
+			name = "Highlighting",
+			description = "Settings for when to highlight and how.",
+			position = 20
+	)
+	String heading_highlighting = "heading_highlighting";
+	@ConfigSection(
+			name = "Yourself",
+			description = "Settings for your character.",
+			position = 30
+	)
+	String heading_yourself = "heading_yourself";
+
+	@ConfigSection(
+			name = "Attachable Enemies",
+			description = "Settings for your enemies.",
+			position = 40
+	)
+	String heading_enemies = "heading_enemies";
+
+	@ConfigSection(
+			name = "Minimap",
+			description = "Settings for your minimap.",
+			position = 50
+	)
+	String heading_minimap = "heading_minimap";
+
+
+
+
+
+
+	@ConfigItem(
+			keyName 	= "highlightInaSafeSpot",
+			name 		= "Highlight When In A Safe Spot?",
+			description = "When you are not in a PVP world, do you want to highlight even in a safe zone?",
+			position 	= 10,
+			section 	= heading_highlighting
+	)
+	default boolean highlightInaSafeSpot() {return true;}
+
+	@ConfigItem(
+			keyName 	= "highlightMode",
+			name 		= "When",
+			description = "When should highlighting be activated?",
+			position 	= 20,
+			section 	= heading_highlighting
+	)
+	default HighlightMode highlightMode()
+	{
+		return HighlightMode.ENEMIES_ARE_NEAR;
+	}
+
+
+
+
+
+
+	@ConfigItem(
+			keyName 	= "playerSafeColor",
+			name 		= "You're safe",
+			description = "When attackable enemies are nearby, this is<br>the color you glow when you are in a safe zone.",
+			position 	= 20,
+			section 	= heading_yourself
+	)
+	default Color playerSafeColor() {return Color.GREEN;}
+	@ConfigItem(
+			keyName 	= "playerAttackableColor",
+			name 		= "You're attackable",
+			description = "When attackable enemies are nearby this is<br>the color you glow when you are NOT in a safe zone<br>and can be attacked.",
+			position 	= 30,
+			section 	= heading_yourself
+	)
+	default Color playerAttackableColor() {return Color.RED;}
+	@ConfigItem(
+			keyName 	= "yourPlayersGlowWidth",
+			name 		= "Your Glow Width",
+			description = "How thick do we draw your players width for the glowing?",
+			position 	= 40,
+			section 	= heading_yourself
+	)
+	default int yourPlayersGlowWidth() {return 3;}
+
+
+
+
+
+
+	@ConfigItem(
+			keyName 	= "enemyEasyColor",
+			name 		= "Enemy Easy",
+			description = "",
+			position 	= 50,
+			section 	= heading_enemies
+	)
+	default Color enemyEasyColor() {return Color.GREEN;}
+	@ConfigItem(
+			keyName 	= "enemyEqual",
+			name 		= "Your equal",
+			description = "",
+			position 	= 60,
+			section 	= heading_enemies
+	)
+	default Color enemyEqual() { return new Color(50, 218, 150);}
+	@ConfigItem(
+			keyName 	= "enemyChallengingColor",
+			name 		= "Enemy Challenging",
+			description = "",
+			position 	= 70,
+			section 	= heading_enemies
+	)
+	default Color enemyChallengingColor() {return Color.YELLOW;}
+	@ConfigItem(
+			keyName 	= "enemyHardColor",
+			name 		= "Enemy Hard",
+			description = "",
+			position 	= 70,
+			section 	= heading_enemies
+
+	)
+	default Color enemyHardColor() {return Color.ORANGE;}
+	@ConfigItem(
+			keyName 	= "enemyExtremeColor",
+			name 		= "Enemy EXTREME",
+			description = "",
+			position 	= 80,
+			section 	= heading_enemies
+
+	)
+	default Color enemyExtremeColor() {return new Color(170, 0, 0);}
+	@ConfigItem(
+			keyName 	= "enemiesGlowWidth",
+			name 		= "Enemy Glow Width",
+			description = "How thick do we draw the width of enemie's glow?",
+			position 	= 80,
+			section 	= heading_enemies
+	)
+	default int enemiesGlowWidth() {return 1;}
+
+
+
+
+
+	@ConfigItem(
+			keyName 	= "dotBackgroundColor",
+			name 		= "Dot Background",
+			description = "On the minimap attackable enemies have a background color, this is it.",
+			position 	= 10,
+			section 	= heading_minimap
+	)
+	default Color dotBackgroundColor() {return Color.BLACK;}
+	@ConfigItem(
+			keyName 	= "dotWidth",
+			name 		= "Dot's width",
+			description = "How thick do we make the dots on the minimap?",
+			position 	= 20,
+			section 	= heading_minimap
+	)
+	default int dotWidth() {return 8;}
+
+
+
+
+
+
+
 }
--- src/main/java/com/mycombatlevel/ExamplePlugin.java
+++ src/main/java/com/mycombatlevel/ExamplePlugin.java
@@ -1,38 +1,34 @@
 package com.mycombatlevel;
 
-import com.google.inject.Provides;
 import net.runelite.api.*;
 import net.runelite.api.coords.LocalPoint;
-import net.runelite.api.events.MenuEntryAdded;
+import net.runelite.api.events.*;
+import net.runelite.api.gameval.InterfaceID;
 import net.runelite.api.widgets.Widget;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
-import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.OverlayManager;
-import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.ui.overlay.outline.ModelOutlineRenderer;
 
-import net.runelite.api.MenuAction;
-import net.runelite.api.MenuEntry;
-import net.runelite.api.Player;
-import net.runelite.client.eventbus.Subscribe;
-
+import com.google.inject.Provides;
 import javax.inject.Inject;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.PluginDescriptor;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
-
-
+import java.util.ArrayList;
+import java.util.List;
 
 
 @PluginDescriptor(
 		name = "My Combat Level"
 )
-public class ExamplePlugin extends Plugin
-{
+public class ExamplePlugin extends Plugin {
 	@Inject
 	private Client client;
 
@@ -46,197 +42,222 @@ public class ExamplePlugin extends Plugin
 	private ModelOutlineRenderer modelOutlineRenderer;
 
 
-	private int combatLevel;
+
+
+
 	private int attackMin;
 	private int attackMax;
-	private int wildernessLevel;
+	private boolean validationToDraw;
 
-	private boolean isThereSomeoneToAttack;
-	private boolean isPvpWorld;
-	private boolean inWilderness;
-	private boolean inSafeZone;
+	private Player localPlayer;
+	private List<Player> allEnemies = new ArrayList<>();
 
-	private final MyOverlay overlay = new MyOverlay();
-	private final MySceneOverlay overmysceneoverlay = new MySceneOverlay();
+	private boolean inSafeZone		= true;
+	private int myCombatLevel;
+	private int wildernessLevel 	= 0;
+	private int lastWildernessLevel = 0;
+	private boolean isPvpWorld 		= false;
+
+	private final overlay_minimap 	overlay_minimap 	= new overlay_minimap();
+	private final overlay_screen 	overlay_screen 		= new overlay_screen();
+
+
+
+
 
 	@Override
-	protected void startUp()
-	{
-		overlayManager.add(overlay);
-		overlayManager.add(overmysceneoverlay);
+	protected void startUp() {
+		overlayManager.add(overlay_minimap);overlayManager.add(overlay_screen);
+
+		calculateLocalAttackRange();
+		allEnemies	= calculateAllEnemies();
+		isPvpWorld 	= WorldType.isPvpWorld(client.getWorldType());
 	}
 
 	@Override
-	protected void shutDown()
-	{
-		overlayManager.remove(overlay);
-		overlayManager.remove(overmysceneoverlay);
-	}
+	protected void shutDown() { overlayManager.remove(overlay_minimap);overlayManager.remove(overlay_screen); }
+
+
+
+
 
 
 	@Subscribe
-	public void onMenuEntryAdded(MenuEntryAdded event)
-	{
+	private void onPlayerSpawned(PlayerSpawned event) {
+		Player player = event.getPlayer();
+		if(player == null){ 		return; }
+		if(player == localPlayer) { return;}
+
+		calculateLocalAttackRange();
+		int playerCombatLevel = player.getCombatLevel();
+		if (playerCombatLevel >= attackMin && playerCombatLevel <= attackMax) { allEnemies.add(player); }
+	}
+
+	@Subscribe
+	private void onPlayerDespawned(PlayerDespawned event) {
+		allEnemies.remove(event.getPlayer());
+		calculateLocalAttackRange();
+	}
+
+	@Subscribe
+	private void onStatChanged(StatChanged event) {
+		calculateLocalAttackRange();
+		calculateAllEnemies();
+	}
+
+	@Subscribe
+	public void onWorldChanged(WorldChanged event) {
+		isPvpWorld = WorldType.isPvpWorld(client.getWorldType());
+		calculateAllEnemies();
+	}
+
+
+
+
+
+	@Subscribe
+	public void onMenuEntryAdded(MenuEntryAdded event) {
+		if(!validationToDraw){ return; }
 
 		MenuEntry menuEntry = event.getMenuEntry();
-
 		if (menuEntry.getType() != MenuAction.PLAYER_FIRST_OPTION
 				&& menuEntry.getType() != MenuAction.PLAYER_SECOND_OPTION
 				&& menuEntry.getType() != MenuAction.PLAYER_THIRD_OPTION
 				&& menuEntry.getType() != MenuAction.PLAYER_FOURTH_OPTION
-				&& menuEntry.getType() != MenuAction.PLAYER_FIFTH_OPTION)
-		{
-			return;
-		}
+				&& menuEntry.getType() != MenuAction.PLAYER_FIFTH_OPTION) { return; }
+		int identifier 	= menuEntry.getIdentifier();
+		Player player 	= null;
 
-		int identifier = menuEntry.getIdentifier();
-
-		Player player = null;
-
-		for (Player p : client.getPlayers())
-		{
-			if (p != null && p.getId() == identifier)
-			{
-				player = p;
-				break;
+		if(!allEnemies.isEmpty()) {
+			for (Player p : allEnemies) {
+				if (p != null && p.getId() == identifier) {
+					player = p;
+					break;
+				}
 			}
 		}
 
-		if (player == null)
-		{
-			return;
-		}
+		if (player == null) {			return; }
+		if (player == localPlayer) {	return; }
 
-		if (player == client.getLocalPlayer())
-		{
-			return;
-		}
-
-		combatLevel = player.getCombatLevel();
-
-		if (combatLevel >= attackMin && combatLevel <= attackMax)
-		{
+		int playersCombatLevel = player.getCombatLevel();
+		if (playersCombatLevel >= attackMin && playersCombatLevel <= attackMax) {
 
 			String target = menuEntry.getTarget();
-
 			if (menuEntry.getOption().equals("Attack")) {
-				// Remove existing RuneScape color tags
+				// Remove existing RuneScape color tags on the attack field to highlight the whole field
 				target = target.replaceAll("<col=[^>]*>", "");
-				target = target.replaceAll("</col>", "");
+				target = target.replace("</col>", "");
 			}
 
+			Color playersColor 	= getEnemyColor(playersCombatLevel);
+			String hex 			= String.format("%02X%02X%02X", playersColor.getRed(), playersColor.getGreen(), playersColor.getBlue());
+
 			menuEntry.setTarget(
-					"<col=ff8000>! " + target + "</col>"
+					"<col=" + hex + ">!" + target + "</col>"
 			);
 		}
 	}
 
 
 
-	private class MyOverlay extends Overlay
-	{
-		MyOverlay()
-		{
-			setPosition(OverlayPosition.DYNAMIC);
-			setLayer(OverlayLayer.ABOVE_WIDGETS);
-		}
 
-		@Override
-		public Dimension render(Graphics2D graphics)
-		{
-			if (client.getGameState() != GameState.LOGGED_IN) { return null; }
+	@Subscribe
+	private void onGameTick(GameTick event) {
+        boolean inWilderness 	= client.getVarbitValue(Varbits.IN_WILDERNESS) == 1;
+		inSafeZone				= !inWilderness; //if we are not in the wilderness then we assume we are safe
+		validationToDraw 		= (inWilderness || config.highlightInaSafeSpot());
 
-			Player localPlayer = client.getLocalPlayer();
-			combatLevel 	= localPlayer.getCombatLevel();
-			wildernessLevel = 0;
-			inSafeZone 	= true;
-			isPvpWorld 	= WorldType.isPvpWorld(client.getWorldType());
-			inWilderness = client.getVarbitValue(Varbits.IN_WILDERNESS) == 1;
-			isThereSomeoneToAttack = false;
+		if (isPvpWorld) {
+			//Widget safeZoneWidget	= client.getWidget(WidgetInfo.PVP_WORLD_SAFE_ZONE);
+			Widget safeZoneWidget	= client.getWidget(InterfaceID.PvpIcons.PVPW_SAFE);
 
+			inSafeZone 				= safeZoneWidget != null && !safeZoneWidget.isHidden();
+			wildernessLevel 		= 15;
 
-			if (isPvpWorld) {
-				Widget safeZoneWidget 	= client.getWidget(WidgetInfo.PVP_WORLD_SAFE_ZONE);
-				inSafeZone 				= safeZoneWidget != null && !safeZoneWidget.isHidden();
-				wildernessLevel 		= 15;
-			}
+			if(!inSafeZone) { validationToDraw = true; }
 
-			if(inWilderness) {
-				Widget wildernessWidget = client.getWidget(WidgetInfo.PVP_WILDERNESS_LEVEL);
-				if (wildernessWidget != null) {
-					String text = wildernessWidget.getText();
-					text 		= text.replace("Level: ", "");
-					if (text != null && !text.isEmpty()) {
+		}else{ wildernessLevel 		= 1; }
+
+		if(inWilderness) {
+			inSafeZone 		= false;
+			Widget wildernessWidget = client.getWidget(InterfaceID.PvpIcons.WILDERNESSLEVEL);
+
+			if (wildernessWidget != null) {
+				String text = wildernessWidget.getText();
+				if (!text.isEmpty()) {
+					text	= text.replace("Level: ", "");
+					if (!text.isEmpty()) {
 						int brIndex = text.indexOf("<br>");
-
-						if (brIndex != -1) {
-							text = text.substring(0, brIndex);
-						}
+						if (brIndex != -1) { text = text.substring(0, brIndex);}
 
 						wildernessLevel += Integer.parseInt(text);
-						inSafeZone = false;
 					}
 				}
 			}
+		}
 
-			if(wildernessLevel == 0){
-				if(config.highlightPlayers()){
-					wildernessLevel = 1;
-				}
-			}
+		//do our safe zone check
+		if(wildernessLevel == 0){if(config.highlightInaSafeSpot()){ wildernessLevel = 1; }}
 
-			attackMin = Math.max(3, combatLevel - wildernessLevel);
-			attackMax = combatLevel + wildernessLevel;
-			if(attackMin == attackMax){
-				attackMin = 0;
-				attackMax = 0;
-			}
-
-			for (Player player : client.getPlayers()) {
-				if (player == localPlayer)
-				{
-					continue;
-				}
-
-				int playerCombatLevel = player.getCombatLevel();
-
-				if (playerCombatLevel >= attackMin && playerCombatLevel <= attackMax)
-				{
-					isThereSomeoneToAttack = true;
-
-					LocalPoint localPoint = player.getLocalLocation();
-
-					if (localPoint == null)
-					{
-						continue;
-					}
+		//if our wilderness level changes then calc our wilderness specific stuff
+		if(wildernessLevel != lastWildernessLevel) {
+			calculateLocalAttackRange();
+			allEnemies = calculateAllEnemies();
+		}lastWildernessLevel = wildernessLevel;
+	}
 
 
-					Point minimapPoint = Perspective.localToMinimap(client, localPoint);
 
-					if (minimapPoint != null)
-					{
-						int x = (int) minimapPoint.getX();
-						int y = (int) minimapPoint.getY();
+	private class overlay_minimap extends Overlay {
+		overlay_minimap() {setPosition(OverlayPosition.DYNAMIC);setLayer(OverlayLayer.ABOVE_WIDGETS);}
 
-						graphics.setColor(Color.BLACK);
-						graphics.fillOval(
-								x - 2,
-								y - 1,
-								5,
-								5
-						);
+		@Override
+		public Dimension render(Graphics2D graphics) {
+			if (client.getGameState() != GameState.LOGGED_IN) { return null; }
 
-						graphics.setColor(Color.ORANGE);
-						graphics.fillOval(
+			if(validationToDraw) {
+				if (!allEnemies.isEmpty()) {
+					for (Player player : allEnemies) {
+						LocalPoint localPoint = player.getLocalLocation();
+						if (localPoint == null) { continue; }
+
+						Point minimapPoint = Perspective.localToMinimap(client, localPoint);
+						if (minimapPoint != null) {
+							int x = minimapPoint.getX();
+							int y = minimapPoint.getY();
+
+							if(config.dotWidth() > 10){
+								x -= 4;
+								y -= 3;
+							}else if(config.dotWidth() > 8){
+								x -= 3;
+								y -= 2;
+							}else if(config.dotWidth() > 6) {
+								x -= 2;
+								y -= 1;
+							}else if(config.dotWidth() > 3){
+								y -= 1;
+								x -= 1;
+							}
+
+							graphics.setColor(config.dotBackgroundColor());
+							graphics.fillOval(
 								x - 1,
 								y,
-								3,
-								3
-						);
+								config.dotWidth(),
+								config.dotWidth()
+							);
+
+							graphics.setColor(getEnemyColor(player.getCombatLevel()));
+							graphics.fillOval(
+								x,
+								y,
+								config.dotWidth() - 2,
+								config.dotWidth() - 2
+							);
+						}
 					}
-
-
 				}
 			}
 
@@ -245,57 +266,40 @@ public class ExamplePlugin extends Plugin
 	}
 
 
-	private class MySceneOverlay extends Overlay
-	{
-		MySceneOverlay()
-		{
-			setPosition(OverlayPosition.DYNAMIC);
-			setLayer(OverlayLayer.ABOVE_SCENE);
-		}
+	private class overlay_screen extends Overlay {
+		overlay_screen() {setPosition(OverlayPosition.DYNAMIC);setLayer(OverlayLayer.ABOVE_SCENE);}
 
 		@Override
-		public Dimension render(Graphics2D graphics)
-		{
-			Player localPlayer = client.getLocalPlayer();
-			isThereSomeoneToAttack = false;
+		public Dimension render(Graphics2D graphics) {
+			if (client.getGameState() != GameState.LOGGED_IN) { return null; }
 
-
-			for (Player player : client.getPlayers())
-			{
-				if (player == localPlayer)
-				{
-					continue;
-				}
-
-				int playerCombatLevel = player.getCombatLevel();
-
-				if (playerCombatLevel >= attackMin && playerCombatLevel <= attackMax)
-				{
-					isThereSomeoneToAttack = true;
-					modelOutlineRenderer.drawOutline(
+			if(validationToDraw) {
+				if (!allEnemies.isEmpty()) {
+					for (Player player : allEnemies) {
+						int playerCombatLevel = player.getCombatLevel();
+						modelOutlineRenderer.drawOutline(
 							player,
-							2,
-							Color.ORANGE,
-							4
-					);
+							config.enemiesGlowWidth(),
+							getEnemyColor(playerCombatLevel),
+							config.enemiesGlowWidth() * 3
+						);
+					}
 				}
 			}
 
 
-			if(isThereSomeoneToAttack && (isPvpWorld || inWilderness || config.highlightPlayers())) {
 
-				Color myColor = Color.GREEN;
-
-				if (!inSafeZone) {
-					myColor = Color.RED;
-				}
-
+            if(validationToDraw && (config.highlightMode() == ExampleConfig.HighlightMode.ALWAYS || !allEnemies.isEmpty())) {
 				if (localPlayer != null) {
+					Color myColor;
+					if (!inSafeZone) {	myColor = config.playerAttackableColor();}
+					else{ 				myColor = config.playerSafeColor(); }
+
 					modelOutlineRenderer.drawOutline(
-							localPlayer,
-							2,
-							myColor,
-							4
+						localPlayer,
+						config.yourPlayersGlowWidth(),
+						myColor,
+						config.yourPlayersGlowWidth()*3
 					);
 				}
 			}
@@ -306,8 +310,63 @@ public class ExamplePlugin extends Plugin
 
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(ExampleConfig.class);
+	ExampleConfig provideConfig(ConfigManager configManager) {return configManager.getConfig(ExampleConfig.class);}
+
+
+
+
+	private Color getEnemyColor(int enemyLevel){
+		int difference = (myCombatLevel - enemyLevel);
+		Color colorChosen;
+
+		//if our difference is positive then we are higher combat then them
+		if(difference == 0 || difference == 1 || difference == 2){
+			colorChosen = config.enemyEqual();
+		}else if(difference >= 3){
+			colorChosen = config.enemyEasyColor();
+		}else{
+			if(difference < -6){
+				colorChosen = config.enemyExtremeColor();
+			}else if(difference < -3){
+				colorChosen = config.enemyHardColor();
+			}
+			else  {
+				colorChosen = config.enemyChallengingColor();
+			}
+		}
+
+		return colorChosen;
 	}
-}
+
+
+	private List<Player> calculateAllEnemies(){
+		var allPlayers 				= client.getTopLevelWorldView().players();
+		allEnemies.clear();
+
+		if (allPlayers != null && allPlayers.iterator().hasNext()) {
+			for (Player player : allPlayers) {
+				if (player == localPlayer) {continue;}
+
+				if(player != null) {
+					int playerCombatLevel = player.getCombatLevel();
+					if (playerCombatLevel >= attackMin && playerCombatLevel <= attackMax) {
+						allEnemies.add(player);
+					}
+				}
+            }
+		}
+
+		return allEnemies;
+	}
+
+
+	private void calculateLocalAttackRange(){
+		localPlayer		= client.getLocalPlayer();
+		if(localPlayer == null){ return; }
+
+		myCombatLevel	= localPlayer.getCombatLevel();
+		attackMin 		= Math.max(3, myCombatLevel - wildernessLevel);
+		attackMax 		= myCombatLevel + wildernessLevel;
+		if(attackMin == attackMax){ attackMin = 0;attackMax = 0; }
+	}
+}//end of main class
